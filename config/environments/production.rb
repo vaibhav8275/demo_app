ffe0749716c2f824c9045810c05d67dab6055b14
Vaibhav Arora <psdtohtmlcloud@gmail.com>
2017-04-22 22:19:38 +0530

    action_mailer host update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # config.action_mailer.default_url_options = { host: 'production', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'salty-gorge-76442.herokuapp.com', port: 3000 }
 end
